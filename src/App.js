08e52957f62562977171574184095812e7882650
Alexander Caley <zanner11030@gmail.com>
2020-09-16 15:50:14 -0700

    added optical flow gif

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import optical_flow from './assets/optical_flow.gif';
 import './App.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Coming Soon!</h1>
+        <img src={optical_flow} alt="logo" />
       </header>
     </div>
   );
