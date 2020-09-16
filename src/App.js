72266bdb50caef14d1d9424e47f413021d2a0cf2
Alexander Caley <zanner11030@gmail.com>
2020-09-16 15:57:55 -0700

    removed image, added in development

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import optical_flow from './assets/optical_flow.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Coming Soon!</h1>
-        <img src={optical_flow} alt="logo" />
+        <h1>In Development, Coming Soon!</h1>
       </header>
     </div>
   );
