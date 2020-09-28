edafa2a24aa4815fdc40f0cf0388e12bf7425a3d
Alexander Caley <zanner11030@gmail.com>
2020-09-28 16:33:58 -0700

    added transition to projects page

--- src/Projects.js
+++ src/Projects.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Thumbnail from "./Thumbnail";
+import { motion } from "framer-motion";
 
 import phone from "./assets/7644.png";
 import amsterdam from "./assets/amsterdam.jpg";
@@ -9,58 +10,76 @@ function Projects() {
   return (
     <div>
       <h1 className="title">Projects</h1>
-      <Container fluid="md">
-        <Row>
-          <Col>
-            <Thumbnail
-              link="/project/hello"
-              image={amsterdam}
-              title="TEST"
-              category="Test App"
-            />
-          </Col>
-          <Col>
-            <Thumbnail
-              link="/project/bye"
-              image={phone}
-              title="TEST"
-              category="Test App"
-            />
-          </Col>
-          <Col>
-            <Thumbnail
-              link="/test"
-              image={phone}
-              title="TEST"
-              category="Test App"
-            />
-          </Col>
-          <Col>
-            <Thumbnail
-              link="/test"
-              image={phone}
-              title="TEST"
-              category="Test App"
-            />
-          </Col>
-          <Col>
-            <Thumbnail
-              link="/test"
-              image={phone}
-              title="TEST"
-              category="Test App"
-            />
-          </Col>
-          <Col>
-            <Thumbnail
-              link="/test"
-              image={phone}
-              title="TEST"
-              category="Test App"
-            />
-          </Col>
-        </Row>
-      </Container>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={{
+          hidden: {
+            scale: 0.8,
+            opacity: 0,
+          },
+          visible: {
+            scale: 1,
+            opacity: 1,
+            transition: {
+              delay: 0.4,
+            },
+          },
+        }}
+      >
+        <Container fluid="md">
+          <Row>
+            <Col>
+              <Thumbnail
+                link="/project/hello"
+                image={amsterdam}
+                title="TEST"
+                category="Test App"
+              />
+            </Col>
+            <Col>
+              <Thumbnail
+                link="/project/bye"
+                image={phone}
+                title="TEST"
+                category="Test App"
+              />
+            </Col>
+            <Col>
+              <Thumbnail
+                link="/test"
+                image={phone}
+                title="TEST"
+                category="Test App"
+              />
+            </Col>
+            <Col>
+              <Thumbnail
+                link="/test"
+                image={phone}
+                title="TEST"
+                category="Test App"
+              />
+            </Col>
+            <Col>
+              <Thumbnail
+                link="/test"
+                image={phone}
+                title="TEST"
+                category="Test App"
+              />
+            </Col>
+            <Col>
+              <Thumbnail
+                link="/test"
+                image={phone}
+                title="TEST"
+                category="Test App"
+              />
+            </Col>
+          </Row>
+        </Container>
+      </motion.div>
     </div>
   );
 }
