3dec748b59fdad144265d5142ee471bda81016d5
Alexander Caley <zanner11030@gmail.com>
2020-10-02 22:17:43 -0700

    finished bio

--- src/Components/Cards/PersonCard.js
+++ src/Components/Cards/PersonCard.js
@@ -32,9 +32,23 @@ class PersonCard extends React.Component {
           >
             Alexander Caley
           </Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+          <Card.Text style={{ textAlign: "left" }}>
+            Hi there! My name is Alexander, I moved to San Francisco from
+            Glendale, CA about 3 years ago looking to change things up. Recently
+            I've graduated with a degree in Computer Science and I'm looking to
+            establish a career in software development. Much of my work
+            experience and academic experience over the past years has enabled
+            me to hone my skills in contributing to the creating of innovative
+            solutions whether that be in an academic environemnt or in a work
+            environemnt. Through working at Apple I've become very comfortable
+            making the best of every sitation and always keeping a solutions
+            based outlook on things, while my time in school has allowed me to
+            meet some great people who I can work alongside in software
+            development and really strengthen both my problem solving and
+            communication skills. Aside from work I spend a lot of my time
+            'onewheeling' through San Francisco and like to spend time at the
+            nearby beach or Golden Gate Park and I really like to travel when I
+            get the chance.
           </Card.Text>
         </Card.Body>
         <ListGroup
