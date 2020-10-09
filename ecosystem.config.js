26f7ee6af875816de27790bb36b8a3be61a554b2
Alexander Caley <zanner11030@gmail.com>
2020-10-09 16:07:12 -0700

    moved reactserver

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "reactServer",
-      script: "./api/reactServer.js",
+      script: "./reactServer.js",
       watch: true,
       ignore_watch: ["node_modules"],
     },
