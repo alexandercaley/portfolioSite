9a7c0164746102572519af98f12bbd65326b44be
Alexander Caley <zanner11030@gmail.com>
2020-10-09 16:18:34 -0700

    cleaned up reactServer args. Cleaned up ecosystem config file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,17 +6,6 @@ module.exports = {
       watch: true,
       ignore_watch: ["node_modules"],
     },
-
-    // {
-    //   name: "react-app",
-    //   script: "npx",
-    //   args: "serve -p 3000 build",
-    //   watch: true,
-
-    //   env_production: {
-    //     NODE_ENV: "production",
-    //   },
-    // },
     {
       name: "contactServer",
       script: "./api/contactServer.js",
@@ -24,17 +13,4 @@ module.exports = {
       ignore_watch: ["node_modules"],
     },
   ],
-
-  // deploy : {
-  //   production : {
-  //     user : 'SSH_USERNAME',
-  //     host : 'SSH_HOSTMACHINE',
-  //     ref  : 'origin/master',
-  //     repo : 'GIT_REPOSITORY',
-  //     path : 'DESTINATION_PATH',
-  //     'pre-deploy-local': '',
-  //     'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-  //     'pre-setup': ''
-  //   }
-  // }
 };
