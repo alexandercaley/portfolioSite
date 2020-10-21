8625a9debfe8b7b745381562539d7ed6953941ac
Alexander Caley <zanner11030@gmail.com>
2020-10-21 12:59:19 -0700

    formatted text of documents

--- src/GoogleAnalytics/analytics.js
+++ src/GoogleAnalytics/analytics.js
@@ -3,21 +3,21 @@ import ReactGA from "react-ga";
 const TRACKING_ID = "UA-145691579-2";
 
 function init() {
-    const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
-    ReactGA.initialize(TRACKING_ID, { debug: isDev });
+  const isDev = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
+  ReactGA.initialize(TRACKING_ID, { debug: isDev });
 }
 
 function sendEvent(payload) {
-    ReactGA.event(payload);
+  ReactGA.event(payload);
 }
 
 function sendPageview(path) {
-    ReactGA.set({ page: path });
-    ReactGA.pageview(path);
+  ReactGA.set({ page: path });
+  ReactGA.pageview(path);
 }
 
 export default {
-    init,
-    sendEvent,
-    sendPageview,
-}
+  init,
+  sendEvent,
+  sendPageview,
+};
