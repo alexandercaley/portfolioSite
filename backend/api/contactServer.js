ebacd68ca2001acceab4e47b40a3f4976a18685e
Alexander Caley <zanner11030@gmail.com>
2020-11-22 22:23:44 -0800

    changed to setHeader

--- backend/api/contactServer.js
+++ backend/api/contactServer.js
@@ -20,12 +20,15 @@ app.use((req, res, next) => {
   ];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Origin", origin);
   }
 
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.header("Access-Control-Allow-Credentials", true);
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", true);
   return next();
 });
 
