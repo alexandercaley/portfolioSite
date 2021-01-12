cc133478739e689bf169e50017c63ba0876ce0b9
Alexander Caley <zanner11030@gmail.com>
2021-01-11 16:38:24 -0800

    prettier cleanp

--- backend/api/contactServer.js
+++ backend/api/contactServer.js
@@ -10,21 +10,24 @@ const port = process.env.PORT || 4000;
 const app = express();
 
 let originAuth = (req, res, next) => {
-  if(req.headers.origin == "https://www.alexandercaley.com") {
+  if (req.headers.origin == "https://www.alexandercaley.com") {
     return next();
   } else {
     res.status(200).json({
       success: false,
     });
   }
-}
+};
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
 app.use((_, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.alexandercaley.com");
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://www.alexandercaley.com"
+  );
 
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -39,30 +42,33 @@ app.get("/status", (_, res) => {
   res.send("API STATUS: Running!");
 });
 
-app.post("/email", [originAuth, (req, res) => {
-  console.log(req.body);
+app.post("/email", [
+  originAuth,
+  (req, res) => {
+    console.log(req.body);
 
-  sendGrid.setApiKey(sendgridKey);
-  const msg = {
-    to: "alexandercaley@gmail.com",
-    from: "me@alexandercaley.com",
-    subject: `Website Contact-${req.body.name}`,
-    text: `Sender: ${req.body.email}\n${req.body.message}`,
-  };
+    sendGrid.setApiKey(sendgridKey);
+    const msg = {
+      to: "alexandercaley@gmail.com",
+      from: "me@alexandercaley.com",
+      subject: `Website Contact-${req.body.name}`,
+      text: `Sender: ${req.body.email}\n${req.body.message}`,
+    };
 
-  sendGrid
-    .send(msg)
-    .then(() => {
-      res.status(200).json({
-        success: true,
+    sendGrid
+      .send(msg)
+      .then(() => {
+        res.status(200).json({
+          success: true,
+        });
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+        res.status(401).json({
+          success: false,
+        });
       });
-    })
-    .catch((err) => {
-      console.log("error: ", err);
-      res.status(401).json({
-        success: false,
-      });
-    });
-}]);
+  },
+]);
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
