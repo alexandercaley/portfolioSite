c1ad95410abbe2fc76c0f14cbdab8a0f1a29253b
Alexander Caley <zanner11030@gmail.com>
2021-02-01 19:20:06 -0800

    changed animation to be on each component of home and projects page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -10,41 +10,78 @@ function Home() {
   return (
     <div>
       <h1 className="title">Alexander Caley</h1>
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.2,
-            },
-          },
-        }}
-      >
-        <Container fluid>
-          <Row xs={1} sm={1} md={2}>
-            <Col>
+
+      <Container fluid>
+        <Row xs={1} sm={1} md={2}>
+          <Col>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: {
+                  scale: 0.8,
+                  opacity: 0,
+                },
+                visible: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.2,
+                  },
+                },
+              }}
+            >
               <div style={{ paddingBottom: "1rem" }}>
                 <PersonCard />
               </div>
-            </Col>
-            <Col>
+            </motion.div>
+          </Col>
+          <Col>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: {
+                  scale: 0.8,
+                  opacity: 0,
+                },
+                visible: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.2,
+                  },
+                },
+              }}
+            >
               <div style={{ paddingBottom: "1rem" }}>
                 <EducationCard />
               </div>
+            </motion.div>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={{
+                hidden: {
+                  scale: 0.8,
+                  opacity: 0,
+                },
+                visible: {
+                  scale: 1,
+                  opacity: 1,
+                  transition: {
+                    delay: 0.2,
+                  },
+                },
+              }}
+            >
               <div style={{ paddingBottom: "1rem" }}>
                 <WorkExpCard />
               </div>
-            </Col>
-          </Row>
-        </Container>
-      </motion.div>
+            </motion.div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- frontend/src/Pages/Projects.js
+++ frontend/src/Pages/Projects.js
@@ -21,7 +21,29 @@ function Projects() {
     return (
       <Row xs={1} sm={2} md={2} lg={3} xl={4}>
         {projectsArray.map((item, index) => {
-          return <Col key={index}>{item}</Col>;
+          return (
+            <Col key={index}>
+              <motion.div
+                initial="hidden"
+                animate="visible"
+                variants={{
+                  hidden: {
+                    scale: 0.8,
+                    opacity: 0,
+                  },
+                  visible: {
+                    scale: 1,
+                    opacity: 1,
+                    transition: {
+                      delay: 0.2,
+                    },
+                  },
+                }}
+              >
+                {item}
+              </motion.div>
+            </Col>
+          );
         })}
       </Row>
     );
@@ -30,27 +52,10 @@ function Projects() {
   return (
     <div>
       <h1 className="title">Projects</h1>
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={{
-          hidden: {
-            scale: 0.8,
-            opacity: 0,
-          },
-          visible: {
-            scale: 1,
-            opacity: 1,
-            transition: {
-              delay: 0.2,
-            },
-          },
-        }}
-      >
-        <Container fluid>
-          <DrawThumbnails />
-        </Container>
-      </motion.div>
+
+      <Container fluid>
+        <DrawThumbnails />
+      </Container>
     </div>
   );
 }
