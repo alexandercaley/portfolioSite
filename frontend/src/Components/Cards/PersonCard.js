cc0b13cfd970e3b0eed6f8ff11a86ff5b36a6999
Alexander Caley <zanner11030@gmail.com>
2021-05-05 11:23:42 -0700

    cleaned up quickAbout

--- frontend/src/Components/Cards/PersonCard.js
+++ frontend/src/Components/Cards/PersonCard.js
@@ -29,26 +29,19 @@ export default function PersonCard(props) {
   }, [imageElement, showText]);
 
   let about = `Hi there! My name is Alexander. In 2017 I moved from Glendale, CA to San Francisco. I recently graduated with a degree in Computer Science and I'm looking to pursue a career in software development. Much of my experience over the past several years has enabled me to hone my development skills in creating innovative solutions in both my academic and work environments.\n As a result of working at Apple, I have gained various skills including making the best of every customer interaction and always maintaining a solution based outlook on things. My academic experience has paved the way for me to strengthen my problem solving and communication skills and also to meet knowledgeable people, several of whom I work with in software development.\n Aside from work, I spend a lot of my free time 'onewheeling' through San Francisco, often going to the beach or Golden Gate Park. Additionally, I love to travel and explore the world when I have the opportunity.`;
+  let quickAboutArray = about.substring(0, quickAboutCount).split("\n");
   let QuickAbout = () => {
     return (
       <>
-        {about
-          .substring(0, quickAboutCount)
-          .split("\n")
-          .map((str, index) => {
-            return (
-              <>
-                {str}
-                {index + 1 <
-                about.substring(0, quickAboutCount).split("\n").length ? (
-                  <br />
-                ) : (
-                  <></>
-                )}
-              </>
-            );
-          })}
-        {!showText ? <>...</> : <></>}
+        {quickAboutArray.map((str, index) => {
+          return (
+            <>
+              {str}
+              {index + 1 < quickAboutArray.length ? <br /> : null}
+            </>
+          );
+        })}
+        {!showText ? <>...</> : null}
       </>
     );
   };
