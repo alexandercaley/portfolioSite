fdda2319285db8047fe6582107bb86e1102feb37
Alexander Caley <zanner11030@gmail.com>
2023-07-06 11:00:58 -0700

    Add route to buildServer that redirects to mastodon.

--- frontend/buildServer.js
+++ frontend/buildServer.js
@@ -3,6 +3,9 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, "build")));
+app.get("/mastodon", function (_, res) {
+  res.redirect("https://mastodon.world/@alexandercaley");
+});
 app.get("/*", function (_, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
